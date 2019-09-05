aa91dbf97cb8760d8a00a30bd91c61b41b70faaa
Sam Naish <sam.naish92@googlemail.com>
2019-09-05 21:31:31 +0100

    add past page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,25 @@
 import Layout from "../components/Layout";
+import Link from 'next/link';
+
+const PostLink = props => {
+    return (
+        <li>
+            <Link href="{`/post?title=${props.title}`}">
+                <a>{props.title}</a>
+            </Link>
+        </li>
+    );
+}
 
 export default () => {
     return (
         <Layout>
-            <p>This the main title</p>
+            <h1>This the main title</h1>
+            <ul>
+                <PostLink title="hello Next.js"/>
+                <PostLink title="Learning Next.js"/>
+                <PostLink title="Deploy apps with Zeit"/>
+            </ul>
         </Layout>
-    )
+    );
 }
