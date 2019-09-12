2ab173ee16a4e0edb2a62ba5f4d0a091f82f99ae
Sam Naish <sam.naish92@googlemail.com>
2019-09-12 18:57:22 +0100

    change login page look

--- frontend/components/Login/index.js
+++ frontend/components/Login/index.js
@@ -1,93 +1,101 @@
+import Footer from "../Footer";
 
 export default () => {
     return (
-        <div id="login-page">
-        <div className="login-box">
-            <form method="post">
-                <input className="login-box__input" type="text" placeholder="username" required="required"></input>
-                <input className="login-box__input" type="text" placeholder="password" required="required"></input>
-                <button type="submit" className="login-box__login-button">login</button>
-                <footer className="login-box__footer">
-                    <span>Not registered?</span>
-                    <a className="login-box__create-account" href="#">Create an account</a>
-                </footer>
-            </form>
-        </div>
-        <style>{`
-
-        * {
-          box-sizing: border-box;
-        }
-        
-        body {
-          background-image: url("../frontend/images/selectionbox.jpg");
-        }
-        
-        .login-box {
-          background-color: #ffffff;
-          width: 520px;
-          margin: 30px auto;
-          padding: 60px;
-          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
-        }
-        
-        .login-box__input {
-          margin-bottom: 20px;
-          padding-left: 20px;
-          height: 60px;
-          width: 100%;
-          font-size: 16px;
-          background-color: lightgrey;
-          border: none;
-        }
-        
-        .login-box__login-button {
-          margin-bottom: 20px;
-          padding: 10px;
-          width: 100%;
-          height: 60px;
-          background-color: #052f5f;
-          color: white;
-          display: inline-block;
-          text-decoration: none;
-          border: none;
-          font-size: 16px;
-          transition: background 0.3s ease;
-          text-transform: uppercase;
-          cursor: pointer;
-        }
-        
-        button:hover {
-          opacity: 0.8;
-          background-color: #005377;
-        }
-        
-        .login-box__footer {
-          text-align: center;
-          color: #666;
-        }
-        
-        .login-box__create-account {
-          color: #052f5f;
-          text-decoration: none;
-          border-bottom-width: 0;
-          border-bottom: 1px solid transparent;
-          transition: border 0.3s ease;
-        }
-        
-        .login-box__create-account:hover {
-          border-bottom-color: #052f5f;
-        }
-
-        @media screen and (max-width: 440px) {
-
-            .login-box {
-                width: auto;
-                height: auto;
+        <div className="page">    
+            <div className="login-page">
+            <div className="login-box">
+                <form method="post">
+                    <input className="login-box__input" type="text" placeholder="username" required="required"></input>
+                    <input className="login-box__input" type="text" placeholder="password" required="required"></input>
+                    <button type="submit" className="login-box__login-button">login</button>
+                    <footer className="login-box__footer">
+                        <span className="login-box__caption">Not registered?</span>
+                        <a className="login-box__create-account" href="#">Create an account</a>
+                    </footer>
+                </form>
+            </div>
+            <style>{`
+            
+            .login-page {
+                background-image: url(/static/images/selectionbox.jpg);
+                background-size: cover;
+                background-position: center;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: rgba(0, 0, 0, 0.5);
             }
-        }
+            
+            .login-box {
+                background-color: #cc2936;
+                width: 500px;
+                margin: 20px auto;
+                padding: 50px;
+                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+                border-radius: 10px;  
+            }
+            
+            .login-box__input {
+                margin-bottom: 20px;
+                padding-left: 20px;
+                height: 40px;
+                width: 80%;
+                font-size: 16px;
+                background-color: lightgrey;
+                border: none;
+            }
+            
+            .login-box__login-button {
+                margin-bottom: 20px;
+                padding: 10px;
+                width: 45%;
+                height: 40px;
+                background-color: #052f5f;
+                color: white;
+                display: inline-block;
+                text-decoration: none;
+                border: none;
+                font-size: 16px;
+                transition: background 0.3s ease;
+                text-transform: uppercase;
+                cursor: pointer;
+                border-radius: 10px;
+            }
+            
+            button:hover {
+                opacity: 0.8;
+                background-color: #005377;
+            }
+            
+            .login-box__footer {
+                text-align: center;
+                color: #666;
+            }
+            
+            .login-box__create-account {
+                color: white;
+                text-decoration: none;
+                border-bottom-width: 0;
+                border-bottom: 1px solid transparent;
+                transition: border 0.3s ease;
+            }
+            
+            .login-box__create-account:hover {
+                border-bottom-color: #052f5f;
+            }
 
-        `}</style>
+            @media screen and (max-width: 440px) {
+
+                .login-box {
+                    width: auto;
+                    height: auto;
+                }
+            }
+
+            `}</style>
+        </div>
+        <Footer />
     </div>
     );
 }
--- frontend/components/Navigation/index.js
+++ frontend/components/Navigation/index.js
@@ -1,32 +1,39 @@
 export default () => {
   return (
-    <div>
-      <ul className="nav">
-        <li className="nav__link"><a className="nav__anchor" href="/">Home</a></li>
-        <li className="nav__link"><a className="nav__anchor" href="/about">About</a></li>
-        <li className="nav__tag"><a className="nav__anchor" href="">Sign Up</a></li>
-        <li className="nav__tag"><a className="nav__anchor" href="/login">Login</a></li>
-      </ul>
+    <div className="nav">
+      <div className="nav__container">
+        <a className="nav__home"href="/">Meal Planner</a>
+        <ul className="nav__links">
+          <li className="nav__link"><a className="nav__anchor" href="">Sign Up</a></li>
+          <li className="nav__link"><a className="nav__anchor" href="/login">Login</a></li>
+        </ul>
+      </div>
       <style>{`
-
-        body {
-          padding: 0;
-          margin: 0;
-        }
 
         .nav {
           background-color: #ffae03;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          height: 100%;
           margin: 0;
         }
 
-        .nav__link {
+        .nav__container {
+          max-width: 1200px;
+          margin: 0 auto;
+          padding: 0 20px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .nav__links {
           list-style: none;
           justify-content: left;
           padding: 10px;
+        }
+
+        .nav__link {
+          float: left;
+          padding: 0 20px;
         }
 
         .nav__anchor:hover {
