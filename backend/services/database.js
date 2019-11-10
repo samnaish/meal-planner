37acffd33ccc14d1808e2689b62f3a95be2fff93
Sam Naish <sam.naish.92@gmail.com>
2019-11-10 17:49:34 +0000

    use process env variables

--- backend/services/database.js
+++ backend/services/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = {
     connect: () => {
-        return mongoose.createConnection(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@mealplanner-1luis.mongodb.net/mealplanner?retryWrites=true&w=majority`, {
+        return mongoose.createConnection(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`, {
         // return mongoose.createConnection(`mongodb://${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true
